a4d4d0d437bbb211f55ce0eb52b8aa56adcca3f1
andrew wong <awwong92@gmail.com>
2018-06-01 01:22:27 -0400

    ADD: another solution to sumTwoNums

--- customSolutions/easy.js
+++ customSolutions/easy.js
@@ -1,6 +1,7 @@
 // Easy problems
 
-//time: O(n log n)
+//1a.
+//time: O(n log n) -> can still be more optimized if sorted
 //space: O(2) => O(1)
 function sumTwoNums(arr, desiredSum) {
   for (let left = 0; left < arr.length; left++) {
@@ -19,5 +20,27 @@ function sumTwoNums(arr, desiredSum) {
   }
   return []; //pairs dont work, return empty arr
 }
-
 sumTwoNums([1, 2, 3, 4, 5], 8); //[3,5]
+
+//1b.
+//time: O(n) -> goes thru array once and stores all values
+//space: O(n) -> will have differences for every value
+function sumTwoNums2(arr, desiredSum) {
+  let store = {};
+  for (let i = 0; i < arr.length; i++) {
+    let difference = desiredSum - arr[i];
+    if (store[difference]) {
+      return [difference, arr[i]].sort((a, b) => {
+        return a - b;
+      });
+    } else {
+      store[arr[i]] = true;
+    }
+  }
+  return [];
+}
+
+// Do not edit the line below.
+exports.sumTwoNums2 = sumTwoNums2;
+
+sumTwoNums2([1, 2, 3, 4, 5], 8); //[3,5]
