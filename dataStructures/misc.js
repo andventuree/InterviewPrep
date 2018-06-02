d299237255cbc9c43ab28ffcf1ea71312a58579c
andrew wong <awwong92@gmail.com>
2018-06-02 00:38:44 -0400

    LL practice

--- dataStructures/misc.js
+++ dataStructures/misc.js
@@ -1,7 +1,103 @@
-//linked list
+// //linked list
+// class Node {
+//   constructor(value) {
+//     this.value = value;
+//     this.previous = null;
+//     this.next = null;
+//   }
+// }
+
+// class LinkedList {
+//   constructor() {
+//     this.head = null;
+//     this.tail = null;
+//   }
+
+//   addToHead(val) {
+//     let formerHead = this.head; //1) hold onto reference
+//     let newNode = new Node(val); //2) create a new node for val
+
+//     this.head = newNode; //3) assign the new head
+
+//     if (formerHead) {
+//       //4) make the connection
+//       this.head.next = formerHead;
+//       formerHead.previous = this.head;
+//     }
+
+//     if (!this.tail) this.head = this.tail; //5) if theres 1 node, its also the tail
+//   }
+
+//   removeHead() {
+//     let formerHead = this.head; //1) hold onto reference
+//     if (!formerHead) return; //2) if head didnt exist, then just return;
+
+//     if (formerHead.next) {
+//       //3) if head had a next
+//       this.head = formerHead.next; //then make that head
+//       this.head.previous = null; //reset new head's previous to null
+//     } else {
+//       //if no nodes left, reset it entirely
+//       this.head = null;
+//       this.tail = null;
+//     }
+//     return formerHead.value;
+//   }
+
+//   addToTail(val) {
+//     const formerTail = this.tail; //1) hold onto reference
+//     const newNode = new Node(val); //2) create new node for tail
+
+//     this.tail = newNode; //3) reassign tail
+
+//     if (formerTail) {
+//       //4) need to connect them
+//       this.tail.previous = formerTail;
+//       formerTail.next = this.tail;
+//     }
+
+//     if (!this.head) this.head = this.tail; //5) if just 1 node, and head is not assigned yet, then its also the head
+//   }
+
+//   removeTail() {
+//     const formerTail = this.tail; //1) hold onto reference
+//     if (!formerTail) return; //2) if tail didnt exist, then just return
+
+//     if (formerTail.previous) {
+//       //3) sever connection
+//       this.tail = formerTail.previous;
+//       this.tail.next = null;
+//     } else {
+//       //4) no nodes left
+//       this.head = null;
+//       this.tail = null;
+//     }
+
+//     return formerTail.value; //5) just return value
+//   }
+// }
+
+// //BST
+
+// class BST {
+//   constructor(val) {
+//     this.value = val;
+//     this.smaller = null;
+//     this.larger = null;
+//     this.size = 1;
+//   }
+
+//   insert(val) {
+//     const direction = val < this.value ? "smaller" : "larger";
+//     if (this[direction]) this[direction].insert(val);
+//     else this[direction] = new BST(val);
+//     this.size++;
+//   }
+// }
+
 class Node {
-  constructor(value) {
-    this.value = value;
+  constructor(val) {
+    this.value = val;
     this.previous = null;
     this.next = null;
   }
@@ -14,83 +110,55 @@ class LinkedList {
   }
 
   addToHead(val) {
-    let formerHead = this.head; //1) hold onto reference
-    let newNode = new Node(val); //2) create a new node for val
-
-    this.head = newNode; //3) assign the new head
+    const formerHead = this.head; //1) hold onto reference
+    const newNode = new Node(val); //2) create new node for new head
+    this.head = newNode; //3) reassign head
 
     if (formerHead) {
-      //4) make the connection
-      this.head.next = formerHead;
-      formerHead.previous = this.head;
+      //4) make connection
+      this.head.next = formerHead; //connect head with former
+      formerHead.previous = this.head; //connect former with head
     }
-
-    if (!this.tail) this.head = this.tail; //5) if theres 1 node, its also the tail
+    if (!this.tail) this.tail = this.head; //if there's no tail, this new node is the tail as well
   }
 
   removeHead() {
-    let formerHead = this.head; //1) hold onto reference
-    if (!formerHead) return; //2) if head didnt exist, then just return;
+    const removedHead = this.head;
+    if (!removedHead) return;
 
-    if (formerHead.next) {
-      //3) if head had a next
-      this.head = formerHead.next; //then make that head
-      this.head.previous = null; //reset new head's previous to null
+    if (removedHead.next) {
+      this.head = removedHead.next; //new head is former's next
+      this.head.previous = null; //sever connection
     } else {
-      //if no nodes left, reset it entirely
       this.head = null;
       this.tail = null;
     }
-    return formerHead.value;
+    return removedHead.value;
   }
 
   addToTail(val) {
-    const formerTail = this.tail; //1) hold onto reference
-    const newNode = new Node(val); //2) create new node for tail
-
-    this.tail = newNode; //3) reassign tail
+    const formerTail = this.tail;
+    const newNode = new Node(val);
+    this.tail = newNode;
 
     if (formerTail) {
-      //4) need to connect them
       this.tail.previous = formerTail;
       formerTail.next = this.tail;
     }
 
-    if (!this.head) this.head = this.tail; //5) if just 1 node, and head is not assigned yet, then its also the head
+    if (!this.head) this.head = this.tail;
   }
 
   removeTail() {
-    const formerTail = this.tail; //1) hold onto reference
-    if (!this.tail) return; //2) if tail didnt exist, then just return
-
-    if (formerTail.previous) {
-      //3) sever connection
-      this.tail = formerTail.previous;
+    const removedTail = this.tail;
+    if (!removedTail) return;
+    if (removedTail.previous) {
+      this.tail = removedTail.previous;
       this.tail.next = null;
     } else {
-      //4) no nodes left
       this.head = null;
       this.tail = null;
     }
-
-    return formerTail.value; //5) just return value
-  }
-}
-
-//BST
-
-class BST {
-  constructor(val) {
-    this.value = val;
-    this.smaller = null;
-    this.larger = null;
-    this.size = 1;
-  }
-
-  insert(val) {
-    const direction = val < this.value ? "smaller" : "larger";
-    if (this[direction]) this[direction].insert(val);
-    else this[direction] = new BST(val);
-    this.size++;
+    return removedTail.value;
   }
 }
