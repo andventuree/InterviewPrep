45b9f596b88288c48afa48265a88a9349ba23ed4
andrew wong <awwong92@gmail.com>
2018-06-03 00:19:10 -0400

    BST practice

--- dataStructures/misc.js
+++ dataStructures/misc.js
@@ -95,86 +95,100 @@
 //   }
 // }
 
-class Node {
-  constructor(val) {
-    this.value = val;
-    this.previous = null;
-    this.next = null;
-  }
-}
+// class Node {
+//   constructor(val) {
+//     this.value = val;
+//     this.previous = null;
+//     this.next = null;
+//   }
+// }
 
-class LinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-  }
+// class LinkedList {
+//   constructor() {
+//     this.head = null;
+//     this.tail = null;
+//   }
 
-  addToHead(val) {
-    const formerHead = this.head; //1) hold onto reference
-    const newNode = new Node(val); //2) create new node for new head
-    this.head = newNode; //3) reassign head
+//   addToHead(val) {
+//     const formerHead = this.head; //1) hold onto reference
+//     const newNode = new Node(val); //2) create new node for new head
+//     this.head = newNode; //3) reassign head
 
-    if (formerHead) {
-      //4) make connection
-      this.head.next = formerHead; //connect head with former
-      formerHead.previous = this.head; //connect former with head
-    }
-    if (!this.tail) this.tail = this.head; //if there's no tail, this new node is the tail as well
-  }
+//     if (formerHead) {
+//       //4) make connection
+//       this.head.next = formerHead; //connect head with former
+//       formerHead.previous = this.head; //connect former with head
+//     }
+//     if (!this.tail) this.tail = this.head; //if there's no tail, this new node is the tail as well
+//   }
 
-  removeHead() {
-    const removedHead = this.head;
-    if (!removedHead) return;
+//   removeHead() {
+//     const removedHead = this.head;
+//     if (!removedHead) return;
 
-    if (removedHead.next) {
-      this.head = removedHead.next; //new head is former's next
-      this.head.previous = null; //sever connection
-    } else {
-      this.head = null;
-      this.tail = null;
-    }
-    return removedHead.value;
-  }
+//     if (removedHead.next) {
+//       this.head = removedHead.next; //new head is former's next
+//       this.head.previous = null; //sever connection
+//     } else {
+//       this.head = null;
+//       this.tail = null;
+//     }
+//     return removedHead.value;
+//   }
 
-  addToTail(val) {
-    const formerTail = this.tail;
-    const newNode = new Node(val);
-    this.tail = newNode;
+//   addToTail(val) {
+//     const formerTail = this.tail;
+//     const newNode = new Node(val);
+//     this.tail = newNode;
 
-    if (formerTail) {
-      this.tail.previous = formerTail;
-      formerTail.next = this.tail;
-    }
+//     if (formerTail) {
+//       this.tail.previous = formerTail;
+//       formerTail.next = this.tail;
+//     }
 
-    if (!this.head) this.head = this.tail;
-  }
+//     if (!this.head) this.head = this.tail;
+//   }
 
-  removeTail() {
-    const removedTail = this.tail;
-    if (!removedTail) return;
-    if (removedTail.previous) {
-      this.tail = removedTail.previous;
-      this.tail.next = null;
-    } else {
-      this.head = null;
-      this.tail = null;
-    }
-    return removedTail.value;
-  }
-}
+//   removeTail() {
+//     const removedTail = this.tail;
+//     if (!removedTail) return;
+//     if (removedTail.previous) {
+//       this.tail = removedTail.previous;
+//       this.tail.next = null;
+//     } else {
+//       this.head = null;
+//       this.tail = null;
+//     }
+//     return removedTail.value;
+//   }
+// }
+
+// class BST {
+//   constructor(val) {
+//     this.value = val;
+//     this.left = null;
+//     this.right = null;
+//     this.size = 1;
+//   }
+
+//   insert(val) {
+//     const direction = val > this.value ? "right" : "left";
+//     if (this[direction]) this[direction].insert(val);
+//     else this[direction] = new Node(val);
+//     this.size++;
+//   }
+// }
 
 class BST {
   constructor(val) {
     this.value = val;
     this.left = null;
     this.right = null;
-    this.size = 1;
   }
 
   insert(val) {
     const direction = val > this.value ? "right" : "left";
     if (this[direction]) this[direction].insert(val);
-    else this[direction] = new Node(val);
-    this.size++;
+    else this[direction] = new BST(val);
   }
 }
