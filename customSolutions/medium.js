14df45c637d429407eaea61675caca2ebe101bf8
andrew wong <awwong92@gmail.com>
2018-06-06 17:29:01 -0400

    ADD: notes to BST construction

--- customSolutions/medium.js
+++ customSolutions/medium.js
@@ -31,19 +31,23 @@ function postOrderTraverse(tree, array) {
   return array;
 }
 
-class BST {
+class TesterBST {
   constructor(val) {
     this.value = val;
     this.left = null;
     this.right = null;
   }
+  //time: O(log n) - uses BST adv by cutting out half
+  //space: O(log n) - b/c recursive, space is extent of call stack
   insert(val) {
     const direction = val > this.value ? "right" : "left";
     if (this[direction]) this[direction].insert(val);
-    else this[direction] = new BST(val);
+    else this[direction] = new TesterBST(val);
     return this;
   }
 
+  //time: O(log n) - uses BST adv by cutting out half
+  //space: O(log n) - b/c recursive, space is extent of call stack
   contains(value) {
     if (this.value === value) return this; //return found node
     const direction = value > this.value ? "right" : "left";
@@ -52,33 +56,9 @@ class BST {
     }
     return false;
   }
-
-  remove(value) {
-    if (this.contains(value)) {
-      let nodeToRemove = this.contains(value);
-      let childrenArr = this.inOrderTraverse(nodeToRemove, []); //arr of saved children
-      // delete nodeToRemove; //need to break reference so it disppears //think LL
-      console.log(childrenArr);
-      console.log(this);
-      childrenArr.forEach(childValue => this.insert(childValue));
-      // console.log(this);
-      return `${value} node was removed`;
-    } else {
-      return "nothing was removed";
-    }
-  }
-
-  inOrderTraverse(tree, array) {
-    if (tree) {
-      this.inOrderTraverse(tree.left, array);
-      array.push(this.value);
-      this.inOrderTraverse(tree.right, array);
-    }
-    return array;
-  }
 }
 
-const testTree = new BST(10)
+const testTree = new TesterBST(10)
   .insert(4)
   .insert(8)
   .insert(6)
@@ -90,5 +70,132 @@ preOrderTraverse(testTree, []);
 postOrderTraverse(testTree, []);
 
 // console.log(testTree.contains(9));
-// console.log(testTree.remove(1));
-console.log(testTree.remove(4));
+
+/* --------------------------------------------------------------------- */
+//2.
+//time and space complexity: see respective methods
+class BST {
+  constructor(val) {
+    this.value = val;
+    this.left = null;
+    this.right = null;
+  }
+
+  //time: O(log n) - uses BST adv
+  //space: O(1) - only stores 1 node at a time "currentNode"
+  insert(val) {
+    let currentNode = this;
+    while (currentNode) {
+      console.log(currentNode);
+      if (val >= currentNode.value) {
+        if (currentNode.right) {
+          currentNode = currentNode.right;
+        } else {
+          currentNode.right = new BST(val);
+          break;
+        }
+      } else if (val < currentNode.value) {
+        if (currentNode.left) {
+          currentNode = currentNode.left;
+        } else {
+          currentNode.left = new BST(val);
+          break;
+        }
+      }
+    }
+    return this;
+  }
+
+  //time: O(log n) - uses BST adv
+  //space: O(1) - only stores 1 node at a time "currentNode"
+  contains(val) {
+    let currentNode = this;
+    while (currentNode) {
+      if (val > currentNode.value) {
+        currentNode = currentNode.right;
+      } else if (val < currentNode.value) {
+        currentNode = currentNode.left;
+      } else {
+        return true; //currentNode.value === val
+      }
+    }
+    return false;
+  }
+  //time: O(n) - worst case, you have to go through entire tree to redistribute
+  //space: O(1) - not recursive so only holds onto 2 node references at a time
+  remove(val, parentNode = null) {
+    let currentNode = this;
+    while (currentNode) {
+      //1st, we need to find the right node (value)
+      //requires exploring left and right
+      if (val > currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.right;
+      } else if (val < currentNode.value) {
+        parentNode = currentNode;
+        currentNode = currentNode.left;
+      } else {
+        //2nd, we've found the right value
+        if (currentNode.left && currentNode.right) {
+          //a) both children exist
+          currentNode.value = currentNode.right.getMinValue();
+          currentNode.right.remove(currentNode.value, currentNode);
+          // edge case: removing parent node
+        } else if (!parentNode) {
+          //no parent implies we've targeted the root node
+          //first, use the left node to replace root node
+          if (currentNode.left) {
+            //reassign current node (root) to be the left node's contents
+            currentNode.value = currentNode.left.value;
+            currentNode.right = currentNode.left.right;
+            currentNode.left = currentNode.left.left;
+            //if left doesnt exist
+            //reassign current node (root) to be the right node's contents
+          } else if (currentNode.right) {
+            currentNode.value = currentNode.right.value;
+            currentNode.left = currentNode.right.left;
+            currentNode.right = currentNode.right.right;
+          }
+          ////You may have noticed if you target the root node,
+          ////parentNode will never be assigned
+          ////so this step will keep going until entire tree gets redistributed again
+
+          //b) we found the node and its parent's left
+        } else if (parentNode.left === currentNode) {
+          //update parent's left to be one of currentNode's children
+          parentNode.left = currentNode.left
+            ? currentNode.left
+            : currentNode.right;
+          //c) we found the node and its parent's right
+        } else if (parentNode.right === currentNode) {
+          //update parent's right to be one of currentNode's children
+          parentNode.right = currentNode.right
+            ? currentNode.right
+            : currentNode.left;
+        }
+      }
+    }
+    return this;
+  }
+}
+
+// Steps to build remove method on BST
+// 1) find the node to remove
+// 2) once node is found, consider the circumstances:
+//   a) no parent - removing the root node, b/c of this,
+//      the entire tree needs to redistribute
+//   b) 2 children (left and right) - find the smallest value and
+//      replace root with smallest value
+//   c) 1 child (left or right) - take whichever available and have parent reference
+//      the child's children instead (this essentially removes target from tree)
+
+let testBST = new BST(10)
+  .insert(5)
+  .insert(4)
+  .insert(3)
+  .insert(1);
+// console.log(testBST);
+// console.log(testBST.contains(6));
+// console.log(testBST.contains(5));
+console.log(testBST.remove(5));
+// testBST.remove(5);
