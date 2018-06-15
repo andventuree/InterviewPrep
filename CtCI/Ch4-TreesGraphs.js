cf751e60ea7175f2d4de568f1e90abf2f0e7ae47
andrew wong <awwong92@gmail.com>
2018-06-15 00:26:23 -0400

    ADD: Notes on trees CtCI ch 4

---
+++ CtCI/Ch4-TreesGraphs.js
@@ -0,0 +1,26 @@
+/* Reading: ch4 - Trees and graphs
+
+When given a tree problem, you must clarify the facts. Incorrectly assuming something will derail the problem.
+Look out for these issues:
+
+Binary Tree vs Binary Search Tree
+BT simply implies the left children are all lower than or equal to current node. Not that there's just 2 children.
+
+Balanced vs Unbalanced
+If your tree is not balanced, describe your algorithm in terms of worst case and average case.
+
+Full and Complete
+Implies that all nodes have 2 children with the exception of leaves.
+Highly unlikely to meet this condition.
+
+Binary Tree Traversal
+Get good at implementing a) in-order, b) post-order c)pre-order
+
+Tree Balancing
+Very rare! You should be familiar with the runtime operations on balanced trees,
+and vaguely familiar with how you might balance a tree.
+
+Tries
+Each path down the tree just makes a word.
+
+*/
