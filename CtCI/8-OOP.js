b37ac78bb56b4cef4f6ffc04fa894e7a188f6717
andrew wong <awwong92@gmail.com>
2018-06-15 19:07:42 -0400

    ADD: OOP lesson

---
+++ CtCI/8-OOP.js
@@ -0,0 +1,16 @@
+/* Reading: ch 8 - Object oriented design
+
+Intention is to see how you write code. Red flags here suggest red flags on the job.
+
+Steps
+1. clarify every thing
+2. Define the core objects
+3. Analyze relationships - Which objs can you inherit from? How general pupose things should be.
+4. Investigate actions
+
+Unlikely to be tested on design patterns but doesn't hurt to know them.
+Singleton class - Ensures that a class has only one instance and ensures access to the instance through the application. E.g., Only 1 instance in the global scope.
+
+Factory method - Offers an interface for creating an instance of a class. Basically a function that will make whatever pre-created class you're asking for in the arguments.
+
+*/
