95e8a658849464d779ca7e387061dca9f5163378
andrew wong <awwong92@gmail.com>
2018-06-16 23:42:20 -0400

    ADD: more heap practice

--- dataStructures/Heap.js
+++ dataStructures/Heap.js
@@ -15,5 +15,113 @@ Complexity:
 
   When building a heap, its more optimal to use sift down than up.
   However, if you implement build heap with `sift up` instead, all the leaves have to move up to root (not the reverse) and that increases complexity to O(n log n).
-
 */
+
+class MinHeap {
+  constructor(array) {
+    this.heap = this.buildHeap(array);
+  }
+
+  buildHeap(array) {
+    const firstParentIdx = Math.floor((array.length - 2) / 2);
+    for (let currentIdx = firstParentIdx; currentIdx >= 0; currentIdx--) {
+      this.siftDown(currentIdx, array.length - 1, array);
+    }
+    return array;
+  }
+
+  //time O(log n) | space O(1)
+  siftUp(currentIdx, heap) {
+    let parentIdx = Math.floor((currentIdx - 1) / 2); //formula for finding parent
+    while (currentIdx > 0 && heap[currentIdx] < heap[parentIdx]) {
+      console.log(
+        "heap[currentIdx], heap[parentIdx]: ",
+        heap[currentIdx],
+        heap[parentIdx]
+      );
+      //if value is less than parent, then swap its position?
+      this.swap(currentIdx, parentIdx, heap);
+      //swap position of parent b/c value is smaller and should be parent now
+      currentIdx = parentIdx;
+      //find new parent idx
+      parentIdx = Math.floor((currentIdx - 1) / 2);
+      console.log("change parentIdx to: ", parentIdx);
+      //loop goes again
+    }
+    console.log(this.heap);
+  }
+
+  //time O(log n) | space O(1)
+  siftDown(currentIdx, endIdx, heap) {
+    let childOneIdx = currentIdx * 2 + 1; //formula for first child
+    while (childOneIdx <= endIdx) {
+      //currentIdx * 2 + 2 = formula for other child
+      const childTwoIdx =
+        currentIdx * 2 + 2 <= endIdx ? currentIdx * 2 + 2 : -1;
+      let idxToSwap;
+      if (childTwoIdx !== -1 && heap[childTwoIdx] < heap[childOneIdx]) {
+        idxToSwap = childTwoIdx;
+      } else {
+        idxToSwap = childOneIdx;
+      }
+      if (heap[idxToSwap] < heap[currentIdx]) {
+        this.swap(currentIdx, idxToSwap, heap);
+        currentIdx = idxToSwap;
+        childOneIdx = currentIdx * 2 + 1;
+      } else {
+        return;
+      }
+    }
+  }
+
+  //time O(1) | space O(1)
+  peak() {
+    //only looks at first node
+    return this.heap[0];
+  }
+
+  //time O(log n) | space O(1)
+  insert(value) {
+    //add value to back of array
+    this.heap.push(value);
+    //since its, at the back, it'll be the child of something,
+    //so it'll need to shift up to necessary node
+    this.siftUp(this.heap.length - 1, this.heap);
+  }
+
+  //time O(log n) | space O(1)
+  remove() {
+    //can only remove the root node so:
+    //swap the first elem to last elemn then pop it off
+    this.swap(0, this.heap.length - 1, this.heap);
+    const valueToRemove = this.heap.pop();
+    //then rebalance tree
+    this.siftDown(0, this.heap.length - 1, this.heap);
+    return valueToRemove;
+  }
+
+  swap(i, j, heap) {
+    const temp = heap[j];
+    heap[j] = heap[i];
+    heap[i] = temp;
+  }
+}
+
+let testMinHeap = new MinHeap([
+  48,
+  12,
+  24,
+  7,
+  8,
+  -5,
+  24,
+  391,
+  24,
+  56,
+  2,
+  6,
+  8,
+  41
+]);
+
+testMinHeap.insert(5);
