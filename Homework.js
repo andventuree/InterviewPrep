7cc6d111509a6aa380f4db229a0b0e1928f8b6a8
andrew wong <awwong92@gmail.com>
2018-06-28 12:08:08 -0400

    EDIT: homework generator

--- Homework.js
+++ Homework.js
@@ -1,9 +1,19 @@
+// types: array, string, stacks, BT, BST, LL, tries, graphs, heaps, DP, recursion, sorting, search
+// source: leetcode, AE, ctci
+
 class Question {
-  constructor(learnedStatus, difficulty, name, source) {
+  constructor(
+    learnedStatus,
+    difficulty = null,
+    name,
+    source = null,
+    type = null
+  ) {
+    this.name = name;
     this.learnedStatus = learnedStatus;
     this.difficulty = difficulty;
-    this.name = name;
     this.source = source;
+    this.type = type;
   }
 }
 
@@ -12,63 +22,77 @@ class QuestionList {
     this._questions = [];
   }
 
-  add(learnedStatus, difficulty, name, source) {
-    this._questions.push(new Question(learnedStatus, difficulty, name, source));
+  //time: O(1) | space: O(1)
+  add(learnedStatus, difficulty, name, source, type) {
+    this._questions.push(
+      new Question(learnedStatus, difficulty, name, source, type)
+    );
     return this;
   }
 
-  generateRand(num) {
-    //get list of completed questions
-    //then give me a random ones from that list
-    let practiceList = this._questions.filter(quest => {
-      if (quest.learnedStatus) return quest;
-    });
-    let max = practiceList.length - 1;
-    let hwQuestions = [];
-    for (let i = 0; i < num; i++) {
-      let questionIdx = Math.floor(Math.random() * max);
-      hwQuestions.push(practiceList[questionIdx]);
+  //time: O(n) | space: O(n)
+  generateRand(num, type, source) {
+    let list = this._questions.filter(quest => quest.learnedStatus);
+    if (type !== undefined) list = list.filter(quest => quest.type === type);
+    if (source !== undefined) {
+      list = list.filter(quest => quest.source === source);
     }
-    let currentDate = new Date().toDateString();
-    console.log(
-      `**************** Homework for ${currentDate} ****************`
-    );
+    let max = list.length;
+    let hwQuestions = [];
+    if (max > 0) {
+      for (let i = 0; i < Math.min(num, max); i++) {
+        let questionNum = Math.floor(Math.random() * max);
+        hwQuestions.push(list[questionNum]);
+      }
+    } else {
+      return console.log(`No questions by these criteria: ${type} ${source}`);
+    }
+
+    let date = this.dateToday();
+    console.log(`**************** Homework for ${date} ****************`);
     console.log(hwQuestions);
+  }
+
+  dateToday() {
+    return new Date().toDateString();
   }
 }
 
 let homeworkTonight = new QuestionList()
-  .add(true, "easy", "Two Number Sum", "AE")
-  .add(true, "easy", "Find Closest Value in BST", "AE")
-  .add(true, "easy", "Depth First Search", "AE")
-  .add(false, "easy", "Nth Fib", "AE")
-  .add(true, "easy", "Binary Search", "AE")
-  .add(false, "easy", "Find 3 Largest Num Sum", "AE")
-  .add(true, "easy", "Bubble Sort", "AE")
-  .add(true, "easy", "Insert Sort", "AE")
-  .add(true, "easy", "Selection Sort", "AE")
-  .add(true, "easy", "Palindrome Check", "AE")
-  .add(true, "easy", "Caesar Cipher Encryptor", "AE")
-  .add(true, "medium", "Three Number Sum", "AE")
-  .add(true, "medium", "Smallest Difference", "AE")
-  .add(true, "medium", "BST Construction", "AE")
-  .add(true, "medium", "Validate BST", "AE")
-  .add(true, "medium", "BST Traversal", "AE")
-  .add(false, "medium", "Invert Binary Tree", "AE")
-  .add(false, "medium", "Max Subset Sum No Adjacent", "AE")
-  .add(true, "medium", "Number of Ways to Make Change", "AE")
-  .add(false, "medium", "Min Number of Coins", "AE")
-  .add(false, "medium", "Levenshtein Distance", "AE")
+  .add(true, "easy", "Two Number Sum", "AE", "array")
+  .add(true, "easy", "Find Closest Value in BST", "AE", "BST")
+  .add(true, "easy", "Depth First Search", "AE", "graphs")
+  .add(false, "easy", "Nth Fib", "AE", "recursion")
+  .add(true, "easy", "Binary Search", "AE", "search")
+  .add(false, "easy", "Find 3 Largest Num Sum", "AE", "search")
+  .add(true, "easy", "Bubble Sort", "AE", "sorting")
+  .add(true, "easy", "Insert Sort", "AE", "sorting")
+  .add(true, "easy", "Selection Sort", "AE", "sorting")
+  .add(true, "easy", "Palindrome Check", "AE", "string")
+  .add(true, "easy", "Caesar Cipher Encryptor", "AE", "string")
+  .add(true, "medium", "Three Number Sum", "AE", "array")
+  .add(true, "medium", "Smallest Difference", "AE", "array")
+  .add(true, "medium", "BST Construction", "AE", "BST")
+  .add(true, "medium", "Validate BST", "AE", "BST")
+  .add(true, "medium", "BST Traversal", "AE", "BST")
+  .add(false, "medium", "Invert Binary Tree", "AE", "BT")
+  .add(false, "medium", "Max Subset Sum No Adjacent", "AE", "DP")
+  .add(true, "medium", "Number of Ways to Make Change", "AE", "DP")
+  .add(false, "medium", "Min Number of Coins", "AE", "DP")
+  .add(false, "medium", "Levenshtein Distance", "AE", "DP")
   .add(true, "medium", "Kadanes Algo - Max Sum", "AE")
-  .add(false, "medium", "Breath-First Search", "AE")
-  .add(false, "medium", "River Sizes", "AE")
-  .add(false, "medium", "Min Heap Construction", "AE")
-  .add(false, "medium", "Remove kth Node from End", "AE")
-  .add(true, "medium", "Permutations", "AE")
-  .add(false, "medium", "Powerset", "AE")
-  .add(false, "medium", "Search in Sorted Matrix", "AE")
-  .add(false, "medium", "Balanced Brackets", "AE")
-  .add(false, "medium", "Longest Palindrome SubString", "AE")
-  .add(false, "medium", "Suffix Trie Construction", "AE");
+  .add(false, "medium", "Breath-First Search", "AE", "graphs")
+  .add(false, "medium", "River Sizes", "AE", "graphs")
+  .add(false, "medium", "Min Heap Construction", "AE", "heaps")
+  .add(false, "medium", "Remove kth Node from End", "AE", "LL")
+  .add(true, "medium", "Permutations", "AE", "recursion")
+  .add(false, "medium", "Powerset", "AE", "recursion")
+  .add(false, "medium", "Search in Sorted Matrix", "AE", "search")
+  .add(false, "medium", "Balanced Brackets", "AE", "stacks")
+  .add(false, "medium", "Longest Palindrome SubString", "AE", "string")
+  .add(false, "medium", "Suffix Trie Construction", "AE", "tries");
 
 homeworkTonight.generateRand(5);
+// homeworkTonight.generateRand(5, "string");
+// homeworkTonight.generateRand(1, "sorting", "AE");
+// homeworkTonight.generateRand(6, "BST", "AE");
