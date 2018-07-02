2b5700ee25386dc329c1e082cf061ffd3a92a76c
andrew wong <awwong92@gmail.com>
2018-07-02 01:36:01 -0400

    EDIT: update pramp solution

--- Pramp/interview.js
+++ Pramp/interview.js
@@ -24,31 +24,35 @@
 // space: O(1)
 
 function shiftedArrSearch(shiftArr, num) {
-  let pivot = findPivotPoint(shiftArr);
-  if (pivot == 0 || num < shiftArr[0]) {
-    return binarySearch(shiftArr, pivot, shiftArr.length - 1, num);
+  //1) use binary search to find pivotIdx
+  let pivotIdx = findPivotPoint(shiftArr);
+  if (pivotIdx == 0 || num < shiftArr[0]) {
+    //if num is smaller than last item, start from back
+    return binarySearch(shiftArr, pivotIdx, shiftArr.length - 1, num);
   }
-  return binarySearch(shiftArr, 0, pivot - 1, num);
+  //2) use binary search to find numIdx
+  return binarySearch(shiftArr, 0, pivotIdx - 1, num);
 }
 
 function findPivotPoint(arr) {
   let begin = 0;
   let end = arr.length - 1;
   while (begin <= end) {
-    let mid = begin + floor((end - begin) / 2);
+    let mid = begin + Math.floor((end - begin) / 2); //find midpoint
     if (mid == 0 || arr[mid] < arr[mid - 1]) {
       return mid;
     }
+    //since we didnt find it, shrink the arr by 1 then reloop
     if (arr[mid] > arr[0]) begin = mid + 1;
     else end = mid - 1;
   }
 
-  return 0;
+  return 0; //turns out it wasn't shfited at all
 }
 
 function binarySearch(arr, begin, end, num) {
   while (begin <= end) {
-    let mid = begin + floor((end - begin) / 2);
+    let mid = begin + Math.floor((end - begin) / 2);
     if (arr[mid] < num) begin = mid + 1;
     else if (arr[mid] == num) return mid;
     else end = mid - 1;
