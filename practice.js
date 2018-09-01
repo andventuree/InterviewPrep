389d7773261b60dae8564e4554753bc7170fc1e9
andrew wong <awwong92@gmail.com>
2018-09-01 02:25:59 -0400

    practice

--- practice.js
+++ practice.js
@@ -74,3 +74,42 @@ const addTwoNumsInLL = (list1, list2) => {
   }
   return total;
 };
+
+
+class Queue {
+  constructor() {
+    this.line = [];
+    this.frontPointer = 0;
+    this.backPointer = 0;
+  }
+
+  add(val) {
+    this.line[this.backPointer] = val;
+    this.backPointer++;
+    return this; //need to return otherwise you can't chain
+  }
+
+  remove() {
+    this.frontPointer++;
+    return this;
+  }
+
+  print() {
+    console.log("full queue: ", this.line);
+    console.log(
+      "current queue",
+      this.line.slice(this.frontPointer, this.backPointer)
+    );
+  }
+}
+
+let testQueue = new Queue()
+  .add(1)
+  .add(2)
+  .remove()
+  .add(3)
+  .add(4)
+  .add(5)
+  .remove();
+
+testQueue.print(); //[3,4,5]
