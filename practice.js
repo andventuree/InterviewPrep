51ab9d0d8ddb908b14469c3fb2d92a15687ddd0d
andrew wong <awwong92@gmail.com>
2018-09-16 23:58:43 -0400

    practice

--- practice.js
+++ practice.js
@@ -318,3 +318,23 @@ function swap(i, j, arr) {
 }
 
 quickSort([12, 31, 23, 12, 31, 231, 2012, 30, 453254, 2]);
+
+
+function sumTwoNums(arr, desiredSum) {
+  for (let left = 0; left < arr.length; left++) {
+    let currentSum;
+    for (let right = arr.length - 1; right > left; right--) {
+      currentSum = arr[left] + arr[right];
+      console.log(`testing pair:`, arr[left], arr[right]);
+      if (currentSum === desiredSum) {
+        //return array sorted
+        console.log("found pair!", currentSum);
+        return [arr[left], arr[right]].sort((a, b) => {
+          return a - b;
+        });
+      }
+    }
+  }
+  return []; //pairs dont work, return empty arr
+}
+// sumTwoNums([1, 2, 3, 4, 5], 8); //[3,5]
